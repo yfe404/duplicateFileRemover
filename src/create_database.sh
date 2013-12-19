b06d343244f86179964d23e396ca37d4529e64d1
yafeunteun <yannfeunteun@gmail.com>
2013-12-19 11:24:58 +0100

    Update create_database.sh

--- src/create_database.sh
+++ src/create_database.sh
@@ -3,6 +3,6 @@
 # args : $1 => database name
 # args : $2 => table name
 
-echo -e "create table $2  (filepath varchar(255) primary key, filename varchar(255), lastmodified varchar(255), md5sum varchar(255));\n" | /usr/bin/sqlite3 $1
+echo -e "create table $2  (filepath varchar(255) primary key, filename varchar(255), lastmodified varchar(255), size integer, md5sum varchar(255));\n" | /usr/bin/sqlite3 $1
 
 
